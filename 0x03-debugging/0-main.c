49c8292211656309ba502eba83c691f46e4a5ad1
Glory Odeyemi <59374178+gloryodeyemi@users.noreply.github.com>
2022-09-16 16:47:04 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,9 +7,11 @@
 
 void positive_or_negative(int i)
 {
-        int i;
-
-        i = 98;
+        /** 
+        * int i;
+        *
+        * i = 98;
+        */
         positive_or_negative(i);
 
         return (0);
